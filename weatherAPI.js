2d3d9fab8de16a0df1eb1bfaf96173e315946f69
IvanSavchuk <isavchuk99@gmail.com>
2018-08-31 19:44:07 +0300

    update code

--- weatherAPI.js
+++ weatherAPI.js
@@ -1,11 +1,30 @@
-var apiCall = 'http://api.openweathermap.org/data/2.5/weather?q=Ivano-Frankivsk,UA&appid=38082f3f9beb0b0911ceaab991e39d6f';
 
-$.getJSON(apiCall, weatherCallback);
+function getWeather() {
+    var cityName = $('#cityName').val();
 
-function weatherCallback(weatherData) {
-    console.log(
-        weatherData.name, 
-        weatherData.sys.country, 
-        weatherData.description
-    );
+    var apiCall = 
+    'http://api.openweathermap.org/data/2.5/weather?q='+ 
+    cityName 
+    + '&appid=38082f3f9beb0b0911ceaab991e39d6f';
+    
+    $.getJSON(apiCall, weatherCallback);
+    
+    function weatherCallback(weatherData) {
+        var cityName = weatherData.name;
+        var country = weatherData.sys.country;
+        var description = weatherData.weather.description;
+        
+        $('weatherResponse').append(
+            "The weather in " + cityName + 
+            " " + country + 
+            " is currently " + description
+        );
+    
+        // console.log(
+        //     weatherData.name, 
+        //     weatherData.sys.country, 
+        //     weatherData.weather[0].description
+        // );
+    };
 }
+
